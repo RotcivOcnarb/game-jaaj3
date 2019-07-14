d5b872f199b7e7080fe71a987b370be5873cbf35
Camargod <38335652+Camargod@users.noreply.github.com>
2019-07-14 16:28:49 -0300

    ...
    Adição de animação da cena do banheiro

--- Assets/SoapGenerator.cs
+++ Assets/SoapGenerator.cs
@@ -7,23 +7,30 @@ public class SoapGenerator : MonoBehaviour
 
     public GameObject soapPrefab;
     public MaoBanho maoBanho;
+    public Rect aaaa;
     int bolhas = 0;
     float timer;
     public float delay = .5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        aaaa = GetComponent<Rect>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0) && maoBanho.holdingSoap && bolhas <= 50){
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float mpx = mousePosition.x;
+        float mpy = mousePosition.y;
+        if (Input.GetMouseButton(0) && maoBanho.holdingSoap && bolhas <= 50 && mpx > aaaa.x - 1000 && mpx < aaaa.x  && mpy > aaaa.y - 650 && mpy < aaaa.y + 100)
+        {
 
             Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if(GetComponent<RectTransform>().rect.Contains(mouseWorld)){
+            if(GetComponent<RectTransform>().rect.Contains(mouseWorld))
+            {
                 timer += Time.deltaTime;
 
                 if(timer > delay){
@@ -34,8 +41,14 @@ public class SoapGenerator : MonoBehaviour
                     go.transform.position = mouseWorld;
                     bolhas++; 
                 }
+
             }
 
+        }
+        if (bolhas > 5)
+        {
+            Animator cameraTransition = Camera.main.GetComponent<Animator>();
+            cameraTransition.SetBool("Banho2", true);
         }
     }
 }
