17b93f402e58be02040834e375fd0e8196bbbdf8
Rotciv <rotciv_ocnarb@hotmail.com>
2019-07-20 13:03:40 -0300

    mechi nums troco

---
+++ Assets/Scripts/Lava/CandelabroScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandelabroScript : MonoBehaviour
+{
+    public PersonagemGarotinha garotinha;
+    bool jacaiu;
+    public static bool canTransition;
+    public Sprite[] pecas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jacaiu = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mergeVectors(transform.position, garotinha.transform.position, 1, 0, 0);
+    }
+
+    public void Cai(){
+        if(!jacaiu){
+            gameObject.AddComponent<CircleCollider2D>();//.isTrigger = true;
+            gameObject.AddComponent<Rigidbody2D>();
+            jacaiu = true;
+        }
+    }
+
+    Vector3 mergeVectors(Vector3 a, Vector3 b, float x, float y, float z){
+        return new Vector3(
+            Mathf.Lerp(a.x, b.x, x),
+            Mathf.Lerp(a.y, b.y, y),
+            Mathf.Lerp(a.z, b.z, z)
+        );
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision){
+        if(collision.collider.gameObject == garotinha.gameObject || collision.otherCollider.gameObject == garotinha.gameObject){
+            
+            Debug.Log("ENCOSTROU");
+
+            foreach(Sprite s in pecas){
+
+                GameObject g = new GameObject();
+                g.AddComponent<SpriteRenderer>().sprite = s;
+                g.transform.position = transform.position;
+                g.AddComponent<CircleCollider2D>();
+                g.AddComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle *10, ForceMode2D.Impulse);
+                g.layer = 9;
+
+
+            }
+            garotinha.GetComponent<Animator>().SetTrigger("Morreu");
+            //Destroy(garotinha.GetComponent<Rigidbody2D>());
+            //Destroy(garotinha.GetComponent<CapsuleCollider2D>());
+
+            Destroy(gameObject);
+            canTransition = true;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Lava/PersonagemGarotinha.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemGarotinha : MonoBehaviour
+{
+    Rigidbody2D body;
+    public Collider2D[] chaos;
+    Animator animator;
+    bool vivo = true;
+
+    public Camera mainCamera;
+
+    public CandelabroScript candelabro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    bool noChao = false;
+    for(int i = 0; i < chaos.Length; i ++){
+            if(body.IsTouching(chaos[i])){
+                noChao = true;
+                break;
+            }
+        }
+        mainCamera.transform.position = mainCamera.transform.position + 
+            (transform.position - mainCamera.transform.position) / 10f;
+
+        Vector3 newPos = mainCamera.transform.position;
+
+        newPos.z = -10;
+        newPos.y = 0;
+        if(mainCamera.transform.position.x < 0){
+            newPos.x = 0;
+        }
+        if(mainCamera.transform.position.x > 9){
+            newPos.x = 9;
+        }
+
+        mainCamera.transform.position = newPos;
+
+         if(Input.GetKey(KeyCode.RightArrow) && vivo)
+        {
+            body.velocity = new Vector2(10, body.velocity.y);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow) && vivo)
+        {
+            body.velocity = new Vector2(-10, body.velocity.y);
+        }
+        else{
+            body.velocity = new Vector2(body.velocity.x * 0.9f, body.velocity.y);
+        }
+        if (body.IsTouching(chaos[3]))
+        {
+            vivo = false;
+            candelabro.Cai();
+        }
+        for(int i = 0; i < chaos.Length; i ++){
+            if(body.IsTouching(chaos[i])){
+                noChao = true;
+                break;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && noChao && vivo)
+        {
+            body.AddForce(new Vector2(0,20),ForceMode2D.Impulse);
+        }
+        if(vivo && !noChao){
+            animator.SetBool("PuloBoneca",true);
+        }
+        else
+        {
+            animator.SetBool("PuloBoneca",false);
+        }
+    }
+}
